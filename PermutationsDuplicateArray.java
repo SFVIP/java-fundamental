07f3d83565d3f3c1fcaa43d96fd9f34f3f9042b6
Simon Foo <simonfookp@gmail.com>
2026-06-14 01:01:23 +0800

    feat: Permutations for Array

---
+++ PermutationsDuplicateArray.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermutationsDuplicateArray {
+    public static void main(String[] args) {
+        String[] arr = { "A", "A", "B" };
+        permute(arr);
+    }
+
+    public static void permute(String[] arr) {
+        backtrack(arr, 0);
+    }
+
+    private static void backtrack(String[] arr, int start) {
+        if (start == arr.length) {
+            System.out.println(Arrays.toString(arr));
+            return;
+        }
+
+        Set<String> used = new HashSet<>();
+        for (int i = start; i < arr.length; i++) {
+            if (!used.add(arr[i])) {
+                continue;
+            }
+            swap(arr, start, i);
+            backtrack(arr, start + 1);
+            swap(arr, start, i);
+        }
+    }
+
+    private static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ PermutationsNonDuplicateArray.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class PermutationsNonDuplicateArray {
+    
+    // When:
+    // - Permutations 
+    // - All arrangements
+    // - Generate every ordering
+    public static void main(String[] args) {
+        String[] arr = { "A", "B", "C" };
+        permute(arr);
+    }
+
+    public static void permute(String[] arr) {
+        backtrack(arr, 0);
+    }
+
+    private static void backtrack(String[] arr, int start) {
+        System.out.println();
+        System.out.println("arr is = "+ Arrays.toString(arr) + ", start is = "+ start);
+        if (start == arr.length) {
+            System.out.println("ANSWER IS =>" + Arrays.toString(arr));
+            return;
+        }
+
+        for (int i = start; i < arr.length; i++) {
+            System.out.println("i is: " + i);
+            System.out.println("swap("+Arrays.toString(arr)+", "+start+", "+i+");");
+            swap(arr, start, i);
+            System.out.println("After first swap: " + Arrays.toString(arr));
+            System.out.println("backtrack("+Arrays.toString(arr)+", "+start+" + 1)");
+            backtrack(arr, start + 1);
+            System.out.println("swap("+Arrays.toString(arr) + ", " + start + ", " + i + ");");
+            swap(arr, start, i); // backtrack
+            System.out.println("After second swap: " + Arrays.toString(arr));
+        }
+    }
+
+    private static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
